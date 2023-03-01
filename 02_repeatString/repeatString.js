0a629a7c7aef229ebc5175b701c6218990e7a27c
MWHK <116496317+MWHK@users.noreply.github.com>
2023-03-01 19:00:48 +0800

    Completed repeatString and reverseString exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
-};
-
+function repeatString(str, num) {
+    var message = "";
+    var errorMsg = "ERROR"; 
+    var i;
+    for (i = 0; i < num; i++) {
+      message += str;
+    }
+      if (num>0){
+        return message;
+      }
+      else if (num<0){
+        return errorMsg;
+      }
+      else{
+        return str = "";
+      }
+    
+    repeatString("hey", 3);
+}
+        
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString; 
