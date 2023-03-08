9530dcd0b38c76116ea05180cb8ca087f8e25cd0
MWHK <116496317+MWHK@users.noreply.github.com>
2023-03-08 16:17:53 +0800

    sumAll completed. leap years initial test passed. Further work required to pass other tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (int1, int2) {
+    if (int1 > 0 && int2 > 0 && typeof int1 === 'number' && typeof int2 === 'number') {
+        var total = 0;
+        if (int1 < int2) {
+            for (var i = int1; i <= int2; i++) {
+                total += i;
+            }
+            return total;
+        } else if (int1 > int2) {
+            for (var i = int2; i <= int1; i++) {
+                total += i;
+            }
+            return total;
+        }
+    } else {
+        return 'ERROR'
+     }
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = function(number,divisor) {
+                    if(number%divisor==0 && number%100==0 && number%400==0)
+                    return true
+                    else if (number%'4'==0)
+                      return true;
+                    else return false;
+    };
 
-};
 
 // Do not edit below this line
 module.exports = leapYears;
